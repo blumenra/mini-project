daf7380a4653b6f0d7ac7b65b367e8469e422db3
Alon Blumenreich <alon@mail.com>
2018-02-11 12:53:10 +0200

    Added c files from to_compile folder to git after a little provlam

---
+++ src/c/to_compile/compile.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+
+	printf("Compiling..\n");
+
+	int javaFilesCount = 2;
+	char *javaFilesToCompile[1 + javaFilesCount];
+	unsigned int i = 0;
+
+	javaFilesToCompile[i++] = "javac";
+	javaFilesToCompile[i++] = "src/java/learntree/Main.java";
+	javaFilesToCompile[i++] = "src/java/learntree/Example.java";
+	javaFilesToCompile[i] = NULL;
+
+	
+	// for(; i < argc + 1; i++){
+	// 	args[i] = argv[i-1];
+	// }
+
+	execvp(javaFilesToCompile[0], javaFilesToCompile);
+
+	return 1;
+}
